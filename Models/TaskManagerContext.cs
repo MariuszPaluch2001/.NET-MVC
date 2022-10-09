96c98787d2f263524bda9a384d8df70bd9b3756c
mariuszpaluch2001 <mariuszpaluch001@gmail.com>
2022-10-09 16:57:27 +0100

    Push project.

---
+++ Models/TaskManagerContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace web_app.Models
+{
+    public class TaskManagerContext : DbContext
+    {
+        public TaskManagerContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+    }
+}
---
+++ Repositories/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using web_app.Models;
+
+namespace web_app.Repositories
+{
+    public interface ITaskRepository
+    {
+        TaskModel Get(int taskId);
+        IQueryable<TaskModel> GetAllActive();
+        void Add(TaskModel task);
+        void Update(int taskId, TaskModel task);
+        void Delete(int taskId);
+    }
+}
